f87043b2eed6d6dcb40343b4480adbfa1aa63e7b
quill-a <gabesander@gmail.com>
2025-08-03 00:14:39 -0500

    Implement GetMapping

--- src/main/java/dev/quilla/springmvcboot/HomeController.java
+++ src/main/java/dev/quilla/springmvcboot/HomeController.java
@@ -3,9 +3,14 @@ package dev.quilla.springmvcboot;
 import dev.quilla.springmvcboot.model.Alien;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -22,8 +27,18 @@ public class HomeController {
         return "index";
     }
 
-    
+    @GetMapping("getAliens")
+    public String getAliens(Model model) {
 
+        // Return a short list of "aliens" for prototyping purposes.
+        List<Alien> aliens = Arrays.asList(new Alien(111, "David"), new Alien(222, "Julia"));
+        model.addAttribute("result", aliens);
+        
+        return "showAliens";
+    }
+
+    // Dynamically change what gets passed to the results page by using the ModelAttribute annotation in the method
+    // definition, rather than manually adding the attribute to the model within the method body.
     @PostMapping(value = "addAlien")
     public String addAlien(@ModelAttribute("alien1") Alien alien) {
 
