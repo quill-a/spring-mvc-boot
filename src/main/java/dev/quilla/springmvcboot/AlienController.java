13b9f7ec6abf14b91669167112894b26606fc6a2
quill-a <gabesander@gmail.com>
2025-08-09 00:03:35 -0500

    Implement REST PostMapping

--- src/main/java/dev/quilla/springmvcboot/AlienController.java
+++ src/main/java/dev/quilla/springmvcboot/AlienController.java
@@ -2,24 +2,41 @@ package dev.quilla.springmvcboot;
 
 import dev.quilla.springmvcboot.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
     AlienRepo repo;
 
     @GetMapping("aliens")
-    @ResponseBody
+    //@ResponseBody  // Can simply annotate the class as being a RestController to avoid having this on every method.
     public List<Alien> getAliens() {
 
         List<Alien> aliens = repo.findAll();
 
         return aliens;
+    }
+
+    @GetMapping("alien/{id}")
+    public Alien getAlien(@PathVariable int id) {
+
+        Alien alien = repo.findById(id).orElse(new Alien(0, ""));
+
+        return alien;
+    }
+
+    @PostMapping("alien")
+    public Alien addAlien(Alien alien) {
+
+        repo.save(alien);
+
+        return alien;
     }
 }
